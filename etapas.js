b779e830a8c1f8eac613c8cea834a6329bdde690
Mateus Moura <mateus51moura@gmail.com>
2022-11-29 07:47:24 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'MELHOR JOGADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '10',
+                nome: 'LEONEL MESSI',
+                pais: 'Argentina',
+                fotos: [
+                    {url:'messi.jpeg', legenda: 'Messi'}
+                ]
+            },
+            {
+                numero: '07',
+                nome: 'KYLIAN MBAPPE',
+                pais: 'França',
+                fotos: [
+                    {url:'mbappe.jpeg', legenda: 'Mbappe'}
+                ]
+            },
+            {
+                numero: '30',
+                nome: 'GAVI',
+                pais: 'Espanha',
+                fotos: [
+                    {url:'gavi.jpeg', legenda: 'Gavi'}
+                ]
+            },
+  {
+                numero: '13',
+                nome: 'ENNER VALENCIA',
+                pais: 'Equador',
+                fotos: [
+                    {url:'valencia.jpeg', legenda: 'Valencia'}
+                ]
+            },
+
+  {
+                numero: '09',
+                nome: 'Richarlison',
+                pais: 'Brasil',
+                fotos: [
+                    {url:'richarlison.jpeg', legenda: 'Richarlison'}
+                ]
+            }
+        ]
+    }
+]
